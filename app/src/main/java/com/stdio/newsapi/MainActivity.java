5e0896fc17c0f39a80c6f52756d3c3835fa68121
Stdio <kwork-stdio@mail.ru>
2019-09-26 20:33:38 +0300

    0.0.1

--- app/src/main/java/com/stdio/newsapi/MainActivity.java
+++ app/src/main/java/com/stdio/newsapi/MainActivity.java
@@ -3,12 +3,56 @@ package com.stdio.newsapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tvResult);
+        getData();
+    }
+
+    public void getData() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://newsapi.org/v2/everything?domains=google.com&apiKey=6486799f62994b81929ce7efa8f9a174&language=ru";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject obj;
+
+                        try {
+                            obj = new JSONObject(response);
+                            JSONObject articles = obj.getJSONArray("articles").getJSONObject(0);
+                            tvResult.setText(articles.getString("title"));
+                        } catch (JSONException e) {
+                            System.out.println(e.getMessage());
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("response " + error.getMessage());
+            }
+        });
+        queue.add(stringRequest);
     }
 }
