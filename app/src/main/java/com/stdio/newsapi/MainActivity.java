fcecbb16990015fc6205fd83eb3b107ee8a64f04
Stdio <kwork-stdio@mail.ru>
2019-09-28 20:06:22 +0300

    0.0.5 - long text ReycyclerView

--- app/src/main/java/com/stdio/newsapi/MainActivity.java
+++ app/src/main/java/com/stdio/newsapi/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +22,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.ericliu.asyncexpandablelist.CollectionView;
+import com.ericliu.asyncexpandablelist.CollectionViewCallbacks;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,7 +31,7 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CollectionViewCallbacks<String, News> {
 
     TextView tvResult;
     ArrayList<NewsSpinnerModel> newsModel = new ArrayList<>();
@@ -35,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<SourcesModel> sources = new ArrayList<>();
     String apiKey = "f500d8d177eb4f9981c46c3731f2de70";
     int i = 0;
+    private CollectionView<String, News> mCollectionView;
+    private CollectionView.Inventory<String, News> inventory;
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,100 @@ public class MainActivity extends AppCompatActivity {
         tvResult = findViewById(R.id.tvResult);
         getSources();
         getData();
+
+        mCollectionView = (CollectionView) findViewById(R.id.collectionView);
+        mCollectionView.setCollectionCallbacks(this);
+
+        inventory = new CollectionView.Inventory<>();
+
+        // groupOrdinal dictates the sequence of groups to be displayed in the list,
+        // the groups will be displayed in an ascending order on groupOrdinal
+        int groupOrdinal = 0;
+        CollectionView.InventoryGroup<String, News> group1 = inventory.newGroup(groupOrdinal);
+        News news;
+
+        group1.setHeaderItem(getString(R.string.news_header_top_stories));
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title1));
+        news.setNewsBody(getString(R.string.news_body1));
+        group1.addItem(news);
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title2));
+        news.setNewsBody(getString(R.string.news_body2));
+        group1.addItem(news);
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title_3));
+        news.setNewsBody(getString(R.string.news_body3));
+        group1.addItem(news);
+
+        CollectionView.InventoryGroup<String, News> group2 = inventory.newGroup(2);
+        group2.setHeaderItem(getString(R.string.news_header_world));
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title4));
+        news.setNewsBody(getString(R.string.news_body4));
+        group2.addItem(news);
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title5));
+        news.setNewsBody(getString(R.string.news_body5));
+        group2.addItem(news);
+
+
+
+        CollectionView.InventoryGroup<String, News> group3 = inventory.newGroup(3); // 2 is smaller than 10, displayed second
+        group3.setHeaderItem(getString(R.string.news_header_australia));
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title6));
+        news.setNewsBody(getString(R.string.news_body6));
+        group3.addItem(news);
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title7));
+        news.setNewsBody(getString(R.string.news_body7));
+        group3.addItem(news);
+
+
+        news = new News();
+        news.setNewsTitle(getString(R.string.news_title8));
+        news.setNewsBody(getString(R.string.news_body8));
+        group3.addItem(news);
+
+
+        mCollectionView.updateInventory(inventory);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder newCollectionHeaderView(Context context, int groupOrdinal, ViewGroup parent) {
+        // Create a new view.
+        View v = LayoutInflater.from(context)
+                .inflate(R.layout.header_row_item, parent, false);
+
+        return new TitleHolder(v);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder newCollectionItemView(Context context, int groupOrdinal, ViewGroup parent) {
+        // Create a new view.
+        View v = LayoutInflater.from(context)
+                .inflate(R.layout.text_row_item, parent, false);
+
+        return new NewsItemHolder(v);
+    }
+
+    @Override
+    public void bindCollectionHeaderView(Context context, RecyclerView.ViewHolder holder, int groupOrdinal, String headerItem) {
+        ((TitleHolder) holder).getTextView().setText((String) headerItem);
+    }
+
+    @Override
+    public void bindCollectionItemView(Context context, RecyclerView.ViewHolder holder, int groupOrdinal, News item) {
+        NewsItemHolder newsItemHolder = (NewsItemHolder) holder;
+        newsItemHolder.getTextViewTitle().setText(item.getNewsTitle());
+        newsItemHolder.getTextViewDescrption().setText(item.getNewsBody());
     }
 
     private void getSources() {
@@ -170,4 +271,47 @@ public class MainActivity extends AppCompatActivity {
         });
         queue.add(stringRequest);
     }
+
+    public static class TitleHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textView;
+
+        public TitleHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.title);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    public static class NewsItemHolder extends RecyclerView.ViewHolder {
+
+
+        private final TextView tvTitle;
+        private final TextView tvDescription;
+
+        public NewsItemHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "Element " + getPosition() + " clicked.");
+                }
+            });
+            tvTitle = (TextView) v.findViewById(R.id.title);
+            tvDescription = (TextView) v.findViewById(R.id.description);
+        }
+
+        public TextView getTextViewTitle() {
+            return tvTitle;
+        }
+
+        public TextView getTextViewDescrption() {
+            return tvDescription;
+        }
+    }
+
 }
